78530c00ac3cf7f39ca38090bafb967de4e6bbba
Francesco Wofford <francescowofford@gmail.com>
2015-05-31 17:07:41 +0200

    separate view for login form

--- plasmastore/views/category_showcase.views.php
+++ plasmastore/views/category_showcase.views.php
@@ -47,26 +47,7 @@
           </form>
         </div>
     
-<?php if(!EUser::logged()){ echo "
-      <form class=\"navbar-form navbar-right\">
-        <div class=\"form-group\">
-            <input type=\"text\" class=\"form-control\" name=\"username\" placeholder=\"Username\"> 
-            <input type=\"text\" class=\"form-control\" name=\"password\" placeholder=\"Password\">
-        </div>
-        <button type=\"submit\" class=\"btn btn-default\">Sign In</button>
-      </form>"; } 
-      else {echo "
-      <ul class=\"nav navbar-nav navbar-right\">
-      <li class=\"dropdown\">
-          <a href=\"#\" class=\"dropdown-toggle\" data-toggle=\"dropdown\" role=\"button\" aria-expanded=\"false\"><span class=\"glyphicon glyphicon-user\"></span> Guest_User <span class=\"caret\"></span></a>
-          <ul class=\"dropdown-menu\" role=\"menu\">
-            <li><a href=\"#\"><span class=\"glyphicon glyphicon-log-out\"></span> Logout</a></li>
-            <li><a href=\"#\"><span class=\"glyphicon glyphicon-send\"></span>  My Messages</a></li>
-            <li class=\"divider\"></li>
-            <li><a href=\"#\">  My Account</a></li>
-          </ul>
-          </li>
-          </ul>";} 
+<?php EStructure::view("login")
       ?>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
